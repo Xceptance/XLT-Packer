15a2f5abc928867d136e7c583130f2f21239bc18
Hartmut Arlt <h.arlt@xceptance.net>
2020-09-07 08:19:35 +0200

    Issue #6: Remove ability to prepare multiple independent XLT installations - prepare XLT working directory only once as there is no need to run multiple agent controllers any more

--- scripts/common/xlt-home/start-xlt.sh
+++ scripts/common/xlt-home/start-xlt.sh
@@ -9,51 +9,57 @@ DOWNLOAD_ARCHIVE=xlt.zip
 START_PORT=8500
 PASSWORD_FILE=/home/xlt/.acpass
 
-NONE="none"
-
 ARCHIVE_LOCATION="${1}"
 TARGET_DIR="${2}"
-NUMBER_OF_AC="${3}"
-COMMAND="${4:-NONE}"
+AGENT_DIR="$TARGET_DIR/ac"
 
-# HELP
-if [ "$1" == "--help" ]; then
+function help() {
   echo "Start XLT."
   echo "  xlt-archive-file      :  Archive containing the XLT version to start"
   echo "  target-dir            :  Where to extract the archive to? This will be the working directory for XLT."
-  echo "  number-of-agentctrls  :  How many agent controllers to start?"
-  echo "  noremove              :  Remove old agent controller directories (if any)?"
-  echo "Usage: $SCRIPTNAME <xlt-archive-file> <target-dir> <number-of-agentctlrs> [noremove]"
+  echo "Usage: $SCRIPTNAME <xlt-archive-file> <target-dir>"
+}
+
+function err() {
+  >&2 echo "FATAL: $1"
   exit 1
-fi
+}
 
 
-# Check for at least three input parameter, stop otherwise
-if [ $# -lt 3 ]; then
-  echo "Incorrect command line. Please specify the archive and the number of agent-controllers to install."
-  exit 1
+for arg in $@ ; do
+  case $arg in
+    --help)
+      help
+      exit0
+      ;;
+  esac
+done
+
+# Check for at least two input parameters, stop otherwise
+if [ $# -lt 2 ]; then
+  help
+  echo ""
+  err "Incorrect command line. Please specify the XLT archive and installation directory."
+
 fi
 
 
 # Check existience of archive to unpack
 if [ ! -e $ARCHIVE_LOCATION ]; then
-  echo "$ARCHIVE_LOCATION does not exist. Aborting."
-  exit 1
+  err "$ARCHIVE_LOCATION does not exist. Aborting."
 fi
 
 
 # check target
 if [ ! -d $TARGET_DIR ]; then
-  echo "Target dir '$TARGET_DIR' does not exist. Aborting."
-  exit 1
+  err "Target dir '$TARGET_DIR' does not exist. Aborting."
 fi
 
 
 # can we write?
 touch $TARGET_DIR/test
 if [ "$?" -ne "0" ]; then
-  echo "Target dir '$TARGET_DIR' is not writable. Aborting."
-  exit 1
+  err "Target dir '$TARGET_DIR' is not writable. Aborting."
 fi
 
 echo "Cleaning target dir '$TARGET_DIR'"
@@ -62,79 +68,45 @@ rm -rf $TARGET_DIR/*
 
 # Unzipping
 echo "Unzipping XLT archive..."
-unzip $ARCHIVE_LOCATION -d $TARGET_DIR
-
-
-# Go to target
-cd $TARGET_DIR
+unzip $ARCHIVE_LOCATION \
+  -x "*/doc/*" "*/samples/*" "*/tools/*" "*/etc/*" "*/bin/*.cmd" \
+  -d $TARGET_DIR
 
 
 # Removing temp if existing
-echo "Removing temp dir $TEMP..."
-rm -rf $TEMP
-
+if [ -d $AGENT_DIR ]; then
+  echo "Removing old agent dir $AGENT_DIR ..."
+  rm -rf $AGENT_DIR
+fi
 
 # Renaming install dir
-TEMP=_$AGENT_PREFIX
 echo "Renaming XLT dir..."
-mv xlt-* $TEMP
-
-
-# Cleaning install dir
-echo "Cleaning XLT agent template..."
-rm -rf $TEMP/{doc,samples,tools}
-rm $TEMP/bin/*cmd
-
+mv $TARGET_DIR/xlt-* $AGENT_DIR
 
 # Setting rights
 echo "Setting execution rights..."
-chmod a+x $TEMP/bin/*sh
-
-
-# Remove all agents currently installed
-if [ $COMMAND != "noremove" ]; then
-  for f in `ls -d $AGENT_PREFIX*`; do
-    echo "Removing $f..."
-    rm -rf $f
-  done
-fi
+chmod a+x $AGENT_DIR/bin/*.sh
 
 PASSWORD=""
 if [ -f $PASSWORD_FILE ]; then
-  PASSWORD=`cat $PASSWORD_FILE`
+  PASSWORD=$(< $PASSWORD_FILE)
 fi
-# Creating agents dirs
-for (( i = 0 ; i < $NUMBER_OF_AC ; i++ )); do
-  echo "Copying $TEMP to $AGENT_PREFIX$i..."
-  if [ ! -d $AGENT_PREFIX$i ]; then
-    mkdir $AGENT_PREFIX$i;
-  fi
-  cp -rp $TEMP/* $AGENT_PREFIX$i
 
-  # ok, adjust the agentcontroller properties
-  PORT=`expr $START_PORT + $i`
-  sed -i "s/com.xceptance.xlt.agentcontroller.port = 8500/com.xceptance.xlt.agentcontroller.port = $PORT/g" $AGENT_PREFIX$i/config/agentcontroller.properties
-
-  if [ -n "$PASSWORD" ]; then
-    sed -i "s/^com.xceptance.xlt.agentcontroller.password = .*$/com.xceptance.xlt.agentcontroller.password = $PASSWORD/g" $AGENT_PREFIX$i/config/agentcontroller.properties
-  fi
-done
+# Configure port and password
+sed -i 's/com.xceptance.xlt.agentcontroller.port =.*/com.xceptance.xlt.agentcontroller.port = 8500/g' $AGENT_DIR/config/agentcontroller.properties
+if [ -n "$PASSWORD" ]; then
+  sed -i 's/^com.xceptance.xlt.agentcontroller.password = .*$/com.xceptance.xlt.agentcontroller.password = $PASSWORD/g' $AGENT_DIR/config/agentcontroller.properties
+else
+  sed -i 's/^\(com.xceptance.xlt.agentcontroller.password =.*\)$/#\1/g' $AGENT_DIR/config/agentcontroller.properties
+fi
 
 
-# Removing temp
-echo "Removing temp dir $TEMP..."
-rm -rf $TEMP
-
-
-# Start agents
+# Start agentcontroller
 CURRENT_USER=`whoami`
 echo "Kill current java processes (if any)"
 killall -9 -u $CURRENT_USER java
-for f in `ls -d $AGENT_PREFIX*`; do
-  $f/bin/agentcontroller.sh&
-done
 
-# Return
-cd -
+echo "Starting XLT Agent Controller"
+$AGENT_DIR/bin/agentcontroller.sh&
 
-# That's it
+exit 0
